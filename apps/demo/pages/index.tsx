88a87591affc4401add3b5744d144b95d7eac3bc
Jack Hsu <jack.hsu@gmail.com>
2022-08-19 12:45:23 -0400

    update index page

--- apps/demo/pages/index.tsx
+++ apps/demo/pages/index.tsx
@@ -1,11 +1,7 @@
 import styles from './index.module.css';
 
 export function Index() {
-  return (
-    <div className={styles.page}>
-      Test
-    </div>
-  );
+  return <div className={styles.page}>Test!</div>;
 }
 
 export default Index;
