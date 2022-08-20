0015551ea5dfe0adf65ecd997558c6ecb871f07f
Jack Hsu <jack.hsu@gmail.com>
2022-08-19 21:53:40 -0400

    add feature

--- libs/lib3/src/lib/lib3.tsx
+++ libs/lib3/src/lib/lib3.tsx
@@ -1,5 +1,9 @@
-export function Lib3() {
-  return <h1>Welcome to Lib3!</h1>;
+export function Lib3(props: { showSecret?: boolean }) {
+  if (props.showSecret) {
+    return <h1>This is the secret. Ssshhhh...</h1>;
+  } else {
+    return <h1>Welcome to Lib3!</h1>;
+  }
 }
 
 export default Lib3;
